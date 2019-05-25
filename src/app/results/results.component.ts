bfd1100f2c5192511afe89c3479e3ce4e089b5d7
vlad <vladislav.repkin@gmail.com>
2019-05-26 03:06:23 +0700

    pagination is added

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ExchangeService} from '../exchange.service';
 import {Router} from '@angular/router';
+import {IRate} from '../IRate';
 
 @Component({
   selector: 'app-results',
@@ -10,10 +11,23 @@ import {Router} from '@angular/router';
 export class ResultsComponent {
 
   constructor(private exchangeService: ExchangeService, private router: Router) { }
+  private pageArray: IRate[][] = this.exchangeService.rates.reduce((acc, cur, currentIndex) => {
+    if (currentIndex % 10 === 0) {
+      let newGroup = [cur];
+      acc.push(newGroup);
+    } else {
+      acc[Math.floor(currentIndex / 10)].push(cur);
+    }
+    return acc;
+  }, []);
+  private currentPageIndex = 0;
   public back() {
     this.router.navigateByUrl('');
   }
   public open() {
     this.exchangeService.modalWindowIsShown = true;
+  }
+  test() {
+    console.log(this.pageArray);
   }
 }
