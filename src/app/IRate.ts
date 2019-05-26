5a70450543524497f73d2b18d9b2e65f5b87cb66
vlad <vladislav.repkin@gmail.com>
2019-05-26 16:19:25 +0700

    styles for table are changed, IRates is fixed

--- src/app/IRate.ts
+++ src/app/IRate.ts
@@ -1,7 +1,7 @@
 import {CurrencyEnum} from './currencyEnum';
 export interface IRate {
-    rate: number,
-    date: string,
-    from: CurrencyEnum,
-    to: CurrencyEnum
+    rate: number;
+    date: string;
+    from: CurrencyEnum;
+    to: CurrencyEnum;
 }
